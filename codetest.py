117c62ef352b25f498414b53192f835e004fd2d7
Alex Liu <tianyet1026@gmail.com>
2018-09-05 00:01:06 +0800

    Add file

---
+++ codetest.py
@@ -0,0 +1,14 @@
+passward = 'a123456'
+i = 0
+while i < 3:
+	answer = input('請輸入密碼:(最多輸入三次密碼)')
+	i = i + 1
+	j = 3 - i
+	if answer == passward:
+		print('登入成功!')
+		break
+	elif j==0:
+		print ('您登入失敗了，跳出!!')
+		break
+	else:
+		print('密碼錯誤,您還有',j,'次機會')
